41e04d0c25429ad544d609de0ef29f39cfd40f04
Clem10101998 <clement10101998@gmail.com>
2020-11-06 10:41:28 +0100

    Initial commit

---
+++ app/src/main/java/com/github/clem10101998/androidkotlin4a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.clem10101998.androidkotlin4a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
