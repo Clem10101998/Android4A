caa1c47408c4ac42c4135874f17d20ce118e8ba2
Clem10101998 <clement10101998@gmail.com>
2020-12-26 16:46:16 +0100

    Recyclerview, DataList

--- app/src/main/java/com/github/clem10101998/androidkotlin4a/presentation/main/DataActivity.kt
+++ app/src/main/java/com/github/clem10101998/androidkotlin4a/presentation/main/DataActivity.kt
@@ -2,11 +2,33 @@ package com.github.clem10101998.androidkotlin4a.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.clem10101998.androidkotlin4a.R
+import kotlinx.android.synthetic.main.activity_data.*
 
 class DataActivity : AppCompatActivity() {
+
+    private val valuesList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
+
+        WatchedList()
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.adapter = ListAdapter(valuesList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun addElementList(values1: String){
+        valuesList.add(values1)
+    }
+
+    private fun WatchedList(){
+        for(i in 0 until 100){
+            addElementList("Test $i")
+        }
     }
 }
---
+++ app/src/main/java/com/github/clem10101998/androidkotlin4a/presentation/main/ListAdapter.kt
@@ -0,0 +1,38 @@
+package com.github.clem10101998.androidkotlin4a.presentation.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.clem10101998.androidkotlin4a.R
+import kotlinx.android.synthetic.main.row_layout.view.*
+
+class ListAdapter(private val values: List<String>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        //val View: View = itemView.icon
+        val txtHeader: TextView = itemView.firstLine
+        val txtFooter: TextView = itemView.secondLine
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val name = values[position]
+
+        holder.txtHeader.text = name
+        holder.txtFooter.text = "Footer: " +name
+
+    }
+
+    override fun getItemCount(): Int{
+        return values.size
+    }
+
+
+}
